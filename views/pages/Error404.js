78067e0f142a65b292a960925eb1f87571e1adb0
Sarvagya Bansal <sarvagyabansal@protonmail.com>
2020-09-24 16:02:23 +0530

    added Error404Page

--- views/pages/Error404.js
+++ views/pages/Error404.js
@@ -1,4 +1,16 @@
 let Error404 = {
+   render : async () => {
+       let view =  /*html*/`
+            <section class="section">
+            <h1>No such Page exists</h1>
+            <p>
+                <a href="#/interviews">Please visit Homepage</a>
+            </p>
+            </section>
+       `
+       return view
+   }
+    , after_render: async () => {}
 
 }
 
